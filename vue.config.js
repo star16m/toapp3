bebd4e43f0f9be30f617f651997e7a78886e091c
jskim <jskim@hubilon.com>
2020-03-30 09:30:00 +0900

    임시로 dist directory 를 추가.

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 module.exports = {
-    outputDir: path.resolve(__dirname, '../toapp/src/main/resources/static'),
-    configureWebpack: {
-        resolve: {
-            alias: {
-                vue$: 'vue/dist/vue.esm.js',
-            },
-        },
+  // outputDir: path.resolve(__dirname, '../toapp/src/main/resources/static'),
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.esm.js',
+      },
     },
+  },
 };
